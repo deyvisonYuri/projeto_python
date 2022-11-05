82c5d5b414000cd8bdcc101c764125aa232c6bbb
deyvison <deyvisonyuri28@gmail.com>
2022-11-04 21:02:16 -0300

    alteracao dia 04/11

--- aula4.py
+++ aula4.py
@@ -1,7 +1,5 @@
-from optparse import Values
 import tkinter as tk
 from tkinter import ttk
-from tkinter.tix import Tree
 import mysql.connector
 from tkinter.messagebox import showinfo
 #pip install mysql-connector
@@ -37,106 +35,151 @@ def selecionarUsuarios(janelaUsuarios):
         conn = conexao()
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM usuarios")
-        table= cursor.fetchall()
+        table = cursor.fetchall()
         print('\n Usuarios: ')
 
-        columns= ('id','nome','sobrenome','cidade','estado','data_nascimento')
-        Tree= ttk.Treeview(janelaUsuarios, columns=columns, show= 'headings')
+        columns = ('id','nome','sobrenome','cidade','estado','data_nascimento')
+        tree = ttk.Treeview(janelaUsuarios, columns=columns, show='headings')
 
         #define cabeçalhos
-        Tree.heading('id',text='#')
-        Tree.heading('nome',text= 'nome')
-        Tree.heading('sobrenome',text= 'sobrenome')
-        Tree.heading('cidade',text='cidade')
-        Tree.heading('estado',text='estado')
-        Tree.heading('data_nascimento',text='data de nascimento')
-
-        def item_selected(self):
-                item= Tree.focus()
-        Tree.bind('<<treeviewselect>>', item_selected)
-        Tree.grid(row=0, column=0, sticky=tk.NSEW)
+        tree.heading('id',text='#')
+        tree.heading('nome',text='Nome')
+        tree.heading('sobrenome', text='Sobrenome')
+        tree.heading('cidade',text='Cidade')
+        tree.heading('estado',text='Estado')
+        tree.heading('data_nascimento',text='Data de Nascimento')
         
+        def item_selected(self):
+                item = tree.focus()
+
+                global id_usuario
+                global nome_usuario
+                global sobrenome_usuario
+                global cidade_usuario
+                global estado_usuario
+                global data_usuario
+
+                id_usuario.set(tree.item(item)["values"][0])
+                nome_usuario.set(tree.item(item)["values"][1])
+                sobrenome_usuario.set(tree.item(item)["values"][2])
+                cidade_usuario.set(tree.item(item)["values"][3])
+                estado_usuario.set(tree.item(item)["values"][4])
+                data_usuario.set(tree.item(item)["values"][5])
+
+        tree.bind('<<TreeviewSelect>>', item_selected)
+        tree.grid(row=0, column=0, sticky=tk.NSEW)
+
         #adicionar uma barra de rolagem
-        scrollbar= ttk.Scrollbar(janelaUsuarios,orient=tk.VERTICAL,command=Tree.yview)
-        Tree.configure(yscroll= scrollbar.set)
+        scrollbar = ttk.Scrollbar(janelaUsuarios, orient=tk.VERTICAL,command=tree.yview)
+        tree.configure(yscroll=scrollbar.set)
         scrollbar.grid(row=0, column=1,sticky='ns')
-        usuarios=[]
+
+        usuarios = []
         for row in table:
-                usuarios.append((f'{row[0]}',f'{row[1]}',f'{row[2]}',f'{row[3]}',f'{row[4]}',f'{row[5]}'))
-
+                usuarios.append((f'{row[0]}', f'{row[1]}', f'{row[2]}', f'{row[3]}', f'{row[4]}', f'{row[5]}'))
+        
         for user in usuarios:
-                Tree.insert('',tk.END,values=user)
-
-        #for row in table:
-                
-
-
+                tree.insert('',tk.END,values=user)
 def inserirUsuarios(usuario):
-        con=conexao()
-        cursor=con.cursor()
-        cursor.execute(f"INSERT INTO usuarios(id,nome,sobrenome,cidade,estado,data_nascimento)" f"VALUES('{usuario.id}','{usuario.nome}','{usuario.sobrenome}','{usuario.cidade}','{usuario.estado}','{usuario.data_nascimento}')")
+        con = conexao()
+        cursor = con.cursor()
+        cursor.execute(
+        f"INSERT INTO usuarios(id, nome, sobrenome, cidade, estado, data_nascimento)" 
+        f"VALUES('{usuario.id}','{usuario.nome}','{usuario.sobrenome}','{usuario.cidade}','{usuario.estado}','{usuario.data_nascimento}')")
         con.commit()
         desconectar(con)
+
 
 def abrirTelaUsuarios():
     janelaUsuarios = tk.Toplevel(app)
     selecionarUsuarios(janelaUsuarios)
 
-    lblid=tk.Label(janelaUsuarios,text="informe o Id:"
-                ,font="times"
-                ,bg="white",foreground="black")
-    lblid.place(x=100,y=230)
-    entryid=tk.Entry(janelaUsuarios)
-    entryid.place(x=230,y=235)
+    lblId = tk.Label(janelaUsuarios,text="Informe o Id: "
+                        , font="Times"
+                        ,foreground="black")
+    lblId.place(x=100, y=230)
+
+    entryId = tk.Entry(janelaUsuarios, textvariable=id_usuario)
+    entryId.place(x=230,y=235)
 
     lblNome = tk.Label(janelaUsuarios,text="Informe o seu nome: "
-
             ,font="Times"
             ,bg="white",foreground="black")
     lblNome.place(x=100,y=250)
-    entryNome = tk.Entry(janelaUsuarios)
+
+    entryNome = tk.Entry(janelaUsuarios, textvariable=nome_usuario)
     entryNome.place(x=230,y=255)
     
     lblSobrenome = tk.Label(janelaUsuarios,text="Informe o seu sobrenome: "
             ,font="Times"
             ,bg="white",foreground="black")
     lblSobrenome.place(x=100,y=275)
-    entrySobrenome = tk.Entry(janelaUsuarios)
+
+    entrySobrenome = tk.Entry(janelaUsuarios, textvariable=sobrenome_usuario)
     entrySobrenome.place(x=260, y=275)
 
     lblDataNascimento = tk.Label(janelaUsuarios,text="Informe sua data de nascimento"
             ,font="Times"
             ,bg="white", foreground="black")
     lblDataNascimento.place(x=100, y=300)
-    entryDataNascimento = tk.Entry(janelaUsuarios)
+
+    entryDataNascimento = tk.Entry(janelaUsuarios, textvariable=data_usuario)
     entryDataNascimento.place(x=300, y=300)
 
     lblCidade = tk.Label(janelaUsuarios,text="Informe a sua cidade"
             ,font="Times"
             ,bg="white", foreground="black")
     lblCidade.place(x=100,y=325)
-    entryCidade = tk.Entry(janelaUsuarios)
+
+    entryCidade = tk.Entry(janelaUsuarios, textvariable=cidade_usuario)
     entryCidade.place(x=230,y=325)
 
     lblEstado = tk.Label(janelaUsuarios, text="Informe o estado: "
             ,font="Times"
             ,bg="white",foreground="black")
     lblEstado.place(x=100, y=350)
-    entryEstado = tk.Entry(janelaUsuarios)
+    
+    entryEstado = tk.Entry(janelaUsuarios, textvariable=estado_usuario)
     entryEstado.place(x=230, y=350)
     
     def salvarUsuario():
-        conn = conexao()
-        Usuario= Usuarios(None,entryNome.get(),entrySobrenome.get(),entryCidade.get(),entryEstado.get(),entryDataNascimento.get())
-        inserirUsuarios(Usuario)
-        #print("O nome informado foi: ",entryNome.get())
-        #print("O sobrenome informado foi: ", entrySobrenome.get())
-        #print("A data de nascimento informada foi: ", entryDataNascimento.get())
-        #print("A cidade informada foi: ", entryCidade.get())
-        #print("O estado informado foi: ",entryEstado.get())
+        usuario = Usuarios(None, entryNome.get(), entrySobrenome.get(),entryCidade.get(),
+        entryEstado.get(), entryDataNascimento.get())
+        inserirUsuarios(usuario)
+
+    def deletarUsuarios():
+        con = conexao()
+        if(entryId.get()):
+                cursor = con.cursor()
+                sql = f"DELETE FROM usuarios where id = {entryId.get()}"
+                cursor.execute(sql)
+                con.commit()
+                selecionarUsuarios(janelaUsuarios)
+        else:
+                showinfo(title='Informação', message='Erro, Id não informado')
+    def atualizarUsuarios():
+        con = conexao()
+        if(entryId.get()):
+                usuario = Usuarios(None, entryNome.get(),entrySobrenome.get(),entryCidade.get(),entryEstado.get(),entryDataNascimento.get())
+                cursor= con.cursor()
+                sql = "UPDATE usuarios set nome = %s,sobrenome= %s, cidade = %s, estado = $s, data_nascimento= %s WHERE id= %s"
+                val = (f'{usuario.nome}',f'{usuario.sobrenome}',f'{usuario.cidade}',f'{usuario.estado}', f'{usuario.data_nascimento}',f'{usuario.id}')
+                cursor.execute(sql,val)
+                con.commit()
+                desconectar(con)
+                selecionarUsuarios(janelaUsuarios)
+        else:
+                showinfo(title='informaçao',message='erro, id não informado')        
+
     btnSalvar = tk.Button(janelaUsuarios,width=20
             ,text="Salvar", command=salvarUsuario)
-    btnSalvar.place(x=100,y=400)
+    btnSalvar.place(x=100,y=375)
+
+    btnAtualizar= tk.Button(janelaUsuarios,width=20,text='atualizar',command= deletarUsuarios, state=tk.ACTIVE)
+    btnAtualizar.place(x=250,y=375)
+
+    btnDeletar= tk.Button(janelaUsuarios,width=20,text='deletar',command=deletarUsuarios,state=tk.ACTIVE)
+    btnDeletar.place(x=400,y=375)
     
     #entryNome.insert("end","teste")
     #entryNome.insert("end","tormes")
@@ -147,7 +190,17 @@ def abrirTelaProdutos():
     janelaProduto = tk.Toplevel(app)
     janelaProduto.title("Cadastro de Produtos")
     janelaProduto.geometry("800x600")
+
 app = tk.Tk()
+
+id_usuario = tk.StringVar()
+nome_usuario = tk.StringVar()
+sobrenome_usuario = tk.StringVar()
+cidade_usuario = tk.StringVar()
+estado_usuario = tk.StringVar()
+data_usuario = tk.StringVar()
+
+
 menuPrincipal = tk.Menu(app)
 app.config(menu=menuPrincipal)
 
@@ -159,12 +212,9 @@ fileMenu.add_command(label="Cadastrar Produtos"
 menuPrincipal.add_cascade(label="Funcao"
                         ,menu=fileMenu)
 
-#buttonExample = tk.Button(app, 
-#              text="Create new window",
-#              command=createNewWindow)
-#buttonExample.place(x=100,y=50)
+
 app.title("Sistema Tarumã")
 app.geometry("800x600")
 
 app.mainloop()
-app.destroy()
+#app.destroy()
